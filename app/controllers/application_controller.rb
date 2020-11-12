5ca62c641b4bf681868e76775e10648c0218c481
Tatsuma-Arakawa <tatsuma0742@gmail.com>
2020-11-12 14:44:55 +0900

    fix design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,12 @@ class ApplicationController < ActionController::Base
       redirect_to root_path, alert: 'Guest users cannot be edited or deleted.'
     end
   end
+
+  def after_sign_in_path_for(resource)
+    album_boards_path
+  end
   
   private
-    def sign_in_required
-        redirect_to new_user_session_url unless user_signed_in?
-    end
-
     def configure_permitted_parameters
       added_attrs = [ :email, :username, :password, :password_confirmation, :image, :introduce ]
       devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @albumboards = AlbumBoard.paginate(page: params[:page], per_page: )
+    @albumboards = AlbumBoard.paginate(page: params[:page], per_page: 10)
   end
 end
