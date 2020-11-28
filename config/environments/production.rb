f9b27bd18654e6de20cf225d45f6203ae00a2011
Tatsuma-Arakawa <tatsuma0742@gmail.com>
2020-11-28 11:05:18 +0900

    fix config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,16 +92,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  host = "#{ENV['HEROKU_APPNAME']}.herokuapp.com"
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'sharemusictokyo.herokuapp.com/', protocol: 'https' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.gmail.com',
     :domain         => 'gmail.com',
     :port           => '587',
     :authentication => :login ,
-    :user_name      => ENV['GMAIL_USERNAME'],
-    :password       => ENV['GMAIL_PASSWORD'],
+    :user_name      => Rails.application.credentials.gmail[:GMAIL_USERNAME],
+    :password       => Rails.application.credentials.gmail[:GMAIL_PASSWORD],
     :enable_starttls_auto => true
   }
 end
