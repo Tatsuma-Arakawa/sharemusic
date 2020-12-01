df4c6038f37d09e87ae21609681eb9e1b46f6cd9
Tatsuma-Arakawa <tatsuma0742@gmail.com>
2020-12-01 16:55:38 +0900

    fix environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :amazon
-  config.assets.initialize_on_precompile = false
+  config.active_storage.service = :local
+  # config.assets.initialize_on_precompile = false
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
