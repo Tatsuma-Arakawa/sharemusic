f2dd2a2031fdd14a78987f79015c74b4964e81da
Tatsuma-Arakawa <tatsuma0742@gmail.com>
2020-12-07 15:50:00 +0900

    fix s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,17 @@
-unless Rails.env.development? || Rails.env.test?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1'
-    }
+CarrierWave.configure do |config|
+  require 'carrierwave/storage/abstract'
+  require 'carrierwave/storage/file'
+  require 'carrierwave/storage/fog'
 
-    config.fog_directory  = ENV['S3_BUCKET_NAME']
-    config.cache_storage = :fog
-  end
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+    region: 'ap-northeast-1'
+  }
+
+  config.fog_directory  = 's3-sharemusic'
+  config.asset_host = 'https://s3-sharemusic.s3.amazonaws.com'
 end
