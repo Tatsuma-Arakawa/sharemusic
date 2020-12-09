20da6575ee042f10970553eeb679c080488f295d
Tatsuma-Arakawa <tatsuma0742@gmail.com>
2020-12-09 18:37:56 +0900

    add order(id DESC) for homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @albumboards = AlbumBoard.paginate(page: params[:page], per_page: 10)
+    @albumboards = AlbumBoard.paginate(page: params[:page], per_page: 10).order("id DESC")
   end
 end
