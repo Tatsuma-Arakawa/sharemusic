48fdebdbcf3e52029eba3575d8d7bd46a6d20425
Tatsuma-Arakawa <tatsuma0742@gmail.com>
2020-12-13 17:48:08 +0900

    add requests/notifications_request_spec & requests/rooms_request_spec

--- spec/requests/notifications_request_spec.rb
+++ spec/requests/notifications_request_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Notifications", type: :request do
+  let!(:user) { FactoryBot.create(:user) }
+  describe "GET/notifications/index" do
+    before do
+      sign_in user
+      get notifications_index_path
+    end
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/notifications/index"
-      expect(response).to have_http_status(:success)
+    it "リクエストが成功する" do
+      expect(response).to have_http_status "200"
     end
   end
-
 end
--- spec/requests/rooms_request_spec.rb
+++ spec/requests/rooms_request_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Rooms", type: :request do
+  let!(:user) { FactoryBot.create(:user, email: "test@example.com") }
+  let!(:second_user) { FactoryBot.create(:user, email: "sample@example.com") }
+  let!(:room) { FactoryBot.create(:room) }
+  let!(:entry) { FactoryBot.create(:entry, user_id: user.id, room_id: room.id) }
+  let!(:second_entry) { FactoryBot.create(:entry, user_id: second_user.id, room_id: room.id) }
+  let!(:message) { FactoryBot.create(:message, user_id: user.id, room_id: room.id, content: "テストメッセージ") } 
+  describe "GET/rooms/show" do
+    before do
+      sign_in user
+      get room_path(room.id)
+    end
 
+    it "リクエストが成功する" do
+      expect(response).to have_http_status "200"
+    end
+
+    it "メッセージが表示される" do
+      expect(response.body).to include message.content
+    end
+
+    it "usernameが表示される" do
+      expect(response.body).to include user.username
+      expect(response.body).to include second_user.username
+    end
+  end
 end
