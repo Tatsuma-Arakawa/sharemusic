5a93f09810663e4220a3e8fbafbdb37bdd91bb77
Tatsuma-Arakawa <tatsuma0742@gmail.com>
2020-12-16 22:43:12 +0900

    fix require rails_helper

--- spec/system/board_review_spec.rb
+++ spec/system/board_review_spec.rb
@@ -17,6 +17,24 @@ RSpec.describe "BoardReview", type: :system do
       visit album_board_path(albumboard.id)
     end
 
+    it "レビューの投稿、削除できること" do
+      click_on "レビューを作成する"
+      find('#rating-value', visible: false).set('3')
+      fill_in "board_review[title]", with: "testtitle"
+      fill_in "board_review[content]", with: "testreview"
+      click_on "投稿する"
+      expect(page).to have_content "評価: 3/5点"
+      expect(page).to have_content "testtitle"
+      expect(page).to have_content "testreview"
+      expect(page).to have_content "testuser"
+      link = find('.fa-trash-alt')
+      link.click
+      expect do
+        expect(page.accept_confirm).to eq "本当に削除しますか？"
+        expect(page).to have_content "レビューを削除しました"
+      end
+    end
+
     it "レビューの投稿、マイページに表示、削除できること" do
       click_on "レビューを作成する"
       find('#rating-value', visible: false).set('3')
@@ -32,8 +50,8 @@ RSpec.describe "BoardReview", type: :system do
       expect(page).to have_content "testtitle"
       expect(page).to have_content "testreview"
       expect(page).to have_content "testuser"
-      visit album_board_path(albumboard.id)
-      click_on "レビューを削除"
+      link = find('.fa-trash-alt')
+      link.click
       expect do
         expect(page.accept_confirm).to eq "本当に削除しますか？"
         expect(page).to have_content "レビューを削除しました"
