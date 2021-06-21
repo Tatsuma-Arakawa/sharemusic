67ac94273936968694d3ff6d680fb686145b5ab3
Tatsuma Arakawa <tatsuma0742@gmail.com>
2021-06-21 23:39:08 +0900

    fix notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,7 +4,7 @@ class NotificationsController < ApplicationController
   def index
     @notifications = current_user.passive_notifications.paginate(page: params[:page], per_page: 20).order("id DESC")
     @notifications.where(checked: false).each do |notification|
-      notification.update_attributes(checked: true)
+      notification.update(checked: true)
     end
   end
 
